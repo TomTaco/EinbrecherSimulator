8ea225dbfd95090b60781c7d4d0717979f8cf01f
TomTaco <43515248+TomTaco@users.noreply.github.com>
2019-01-16 01:06:38 +0100

    some changos

--- src/model/framework/SQLController.java
+++ src/model/framework/SQLController.java
@@ -19,7 +19,7 @@ public class SQLController {
             // Erstelle eine Verbindung zu unserer SQL-Datenbank
             con = DriverManager.getConnection("jdbc:mysql://mysql.webhosting24.1blu.de/db85565x2810214?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "s85565_2810214", "kkgbeste");
             stmt = con.createStatement();
-            loadAllTables();
+            //loadAllTables();
         } catch (Exception e) {
             e.printStackTrace();
 
